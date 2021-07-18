499484afcf88a2dc7035d2f7dfea34b9c123b2b0
marlysco <marlysco94@gmail.com>
2021-07-18 03:12:26 -0400

    Updating apiRoutes.js

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,18 +1,68 @@
+// const db = require("../models");
+
+// require("mongoose");
+
+// module.exports = (app) => {
+// //Get all the workouts
+//     app.get("api/workouts", async (req, res) => {
+//         try {
+//          const workoutData= await db.Workout.find({});
+//          res.jason(workoutData);
+//         } catch(error) {
+//             console.log(error)
+//             res.sendStatus(500)
+//         }
+//         });
+// //Add exercise
+//  app.post("/api/workouts", (req, res) => {
+//       db.Workout.create({}).
+//       then(data => res.json(data))
+//       .catch(err => {
+
+//         console.log("error", err);
+
+//         res.json(err);
+
+//       });
+//  })
+
+
+// };
+
 const db = require("../models");
 
 require("mongoose");
 
+
+
+
 module.exports = (app) => {
-//Get all the workouts
-    app.get("/workout", async (req, res) => {
-        try {
-         const workoutData= await db.Workout.find({});
-         res.jason(workoutData);
-        } catch(error) {
-            console.log(error)
-            res.sendStatus(500)
-        }
-        });
+
+
+    //Create a Workout
+    app.post("/api/workouts", (req, res) => {
+        db.Workout.create({})
+        .then(data => res.json(data))
+        .catch(err => {
+            console.log(err);
+            res.sendStatus(500);
+          });
+    });
+
+    //Add Exercise
+    app.put("/api/workouts/:id", (req, res) => {
+       db.Workout.findByIdAndUpdate(req.params.id, {$push: {exercises: req.body}},
+            {new: true, runValidators: true})   
+        .then(data => res.json(data))
+        .catch(err => {
+            console.log(err);
+            res.sendStatus(500);
+          });
+
+    });
+
+
+
 
 
 };
