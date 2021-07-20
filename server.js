216e044c4379438aeab29d94a03871d871d3ba7b
marlysco <marlysco94@gmail.com>
2021-07-20 01:12:21 -0400

    deploying

--- server.js
+++ server.js
@@ -10,10 +10,14 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/budget", {
-  useNewUrlParser: true,
-  useFindAndModify: false
-});
+//Mongo DB and Heroku configuration
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", 
+ {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: false
+  });
 
 // routes
 require("./routes/apiRoutes")(app);
